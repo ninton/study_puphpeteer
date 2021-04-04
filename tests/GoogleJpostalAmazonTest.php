394b843e9eb432e64cf9efa627725d1f938fb9ef
aoki.makoto <aoki_makoto@ninton.co.jp>
2021-04-05 00:26:53 +0900

    PuPHPeteerでE2Eテスト(7) 複数タブ、3つめのタブでJpostal郵便番号から住所自動入力

---
+++ tests/GoogleJpostalAmazonTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ninton\Test;
+
+use Ninton\Test\Libs\Amazon\SearchScenario;
+use Ninton\Test\Libs\Google\SearchScenario as GoogleSearchScenario;
+use Ninton\Test\Libs\Jpostal\Sample1PageScneario;
+
+class GoogleJpostalAmazonTest extends BasePuppeteerTestCase
+{
+    use SearchScenario;
+    use GoogleSearchScenario;
+    use Sample1PageScneario;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->browser->newPage();
+        $this->browser->newPage();
+    }
+
+    public function test_3タブ(): void
+    {
+        $this->selectPage(0);
+        $this->本を検索してカートに入れる('テスト駆動開発');
+        $this->assertカート追加直後の小計('3080');
+
+        $this->本を検索してカートに入れる('実践テスト駆動開発');
+        $this->assertカート追加直後の小計('7700');
+
+        $this->selectPage(1);
+        $this->google検索する('PuPHPeteer');
+        $this->assertGoogle検索結果にあるはず('rialto-php/puphpeteer');
+
+        $this->selectPage(2);
+        $this->jspotal_1000001は東京都千代田区千代田();
+    }
+}
---
+++ tests/Libs/Jpostal/Sample1PageScneario.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ninton\Test\Libs\Jpostal;
+
+trait Sample1PageScneario
+{
+    private function jspotal_1000001は東京都千代田区千代田(): void
+    {
+        $sample1Page = Sample1Page::goto($this->page);
+
+        $sample1Page->type郵便番号の上3桁('100');
+        sleep(2);
+        $this->screenShot();
+
+        $address1 = $sample1Page->get都道府県value();
+        $this->assertEquals('東京都', $address1);
+
+        $address2 = $sample1Page->get市区町村value();
+        $this->assertEquals('千代田区', $address2);
+
+        $sample1Page->type郵便番号の下4桁('0001');
+        usleep(100000);
+        $this->screenShot();
+
+        $address3 = $sample1Page->get町域value();
+        $this->assertEquals('千代田', $address3);
+
+        $sample1Page->type郵便番号の下4桁('0002');
+        usleep(100000);
+        $this->screenShot();
+
+        $address3 = $sample1Page->get町域value();
+        $this->assertEquals('皇居外苑', $address3);
+    }
+}
